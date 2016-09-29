bcd60fd085d61d36856b0c286d1d38c7fba4340b
MisterTaki <ustb_gaoqi@163.com>
2016-09-29 17:49:42 +0800

    完成初版

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,29 +1,61 @@
 $(function(){
   (function(){
+    var bannerSlider = $("#banner .banner-container").owlCarousel({
+      loop:true,
+      items: 1,
+      autoplay: true,
+      animateOut: 'bounceOut',
+      animateIn: 'fadeIn',
+      autoplayTimeout: 3000,
+      mouseDrag: false,
+      touchDrag: false
+    });
+    $('#banner .prev-button').on('click',function(){
+      bannerSlider.trigger('prev.owl.carousel')
+    })
+    $('#banner .next-button').on('click',function(){
+      bannerSlider.trigger('next.owl.carousel')
+    })
+    $("#members").owlCarousel({
+      loop:true,
+      items: 5,
+      autoWidth:true,
+      dots: false,
+      autoplay: true,
+      autoplayHoverPause: true,
+      center: true
+    });
+
+    var $knowMores = $('.know-more');
+    var $knowBacks = $('.back-icon');
+    $knowMores.click(function(){
+      var $front = $(this).parents('.front');
+      $front.addClass('front-hover');
+      $front.siblings('.back').addClass('back-hover');
+      return false;
+    })
+    $knowBacks.click(function(){
+      var $back = $(this).parents('.back');
+      $back.removeClass('back-hover');
+      $back.siblings('.front').removeClass('front-hover');
+      return false;
+    })
+
     var serviceTop = $('#service').offset().top - 150,
         caseTop = $('#case').offset().top - 150,
         teamTop = $('#team').offset().top - 150,
-        navLeft = $('#header .nav').offset().left,
-        curIndex = 0,
         $anchors = $('#header .link'),
         $anchorParents = $('#header .nav li'),
         $curBar = $('#cur-bar'),
-        $headerContainer = $('#header .header-container');
-    
-    // 滑动到锚链接
-    $anchors.click(function () {
-      var hr = $(this).attr("href");
-      var anh = $(hr).offset().top - 80;
-      $("html,body").stop().animate({
-        scrollTop: anh
-      }, 1000);
-      return false;
-    });
+        $headerContainer = $('#header .header-container')
+        curIndex = -1,
+        curFlag = 0;
 
     function getParam(index) {
-      var i = index || curIndex,
-          curWidth = $anchors.eq(i).width(),
-          curLeft = $anchors.eq(i).offset().left - navLeft + 58;
+      var i = typeof (index) == "number" ? index : curIndex,
+          navLeft = $('#header .nav').offset().left,
+          curWidth = $anchors.eq(i).width() + 10,
+          curLeft = $anchors.eq(i).offset().left - navLeft + 52;
       return {
         curWidth: curWidth,
         curLeft: curLeft
@@ -31,17 +63,19 @@ $(function(){
     }
 
     function switchCur(index) {
-      var params = getParam(index);
-      $curBar.stop().animate({
-        left: params.curLeft,
-        width: params.curWidth
-      }, 400);
+      if (!curFlag) {
+        var params = getParam(index);
+        $curBar.stop().animate({
+          left: params.curLeft,
+          width: params.curWidth
+        }, 400);
+      }
     }
 
-
     $anchors.hover(function(){
-      var width = $(this).width(),
-          left = $(this).offset().left - navLeft + 58,
+      var width = $(this).width() + 10,
+          navLeft = $('#header .nav').offset().left,
+          left = $(this).offset().left - navLeft + 52,
           i = $anchors.index($(this));
       $curBar.stop().animate({
         left: left,
@@ -50,17 +84,20 @@ $(function(){
         $anchorParents.eq(i).addClass('active').siblings('li').removeClass('active');
       });
     }, function(){
-      var params = getParam();
-      $curBar.stop().animate({
-        left: params.curLeft,
-        width: params.curWidth,
-      }, 800, function(){
+      if (!curFlag) {
+        var params = getParam();
+        $curBar.stop().animate({
+          left: params.curLeft,
+          width: params.curWidth,
+        }, 800, function(){
         $anchorParents.eq(curIndex).addClass('active').siblings('li').removeClass('active');
       });
-    });
+    }});
 
     function toggleScroll() {
       var scrollTop = $(window).scrollTop();
+      var preIndex = curIndex;
+
       if (scrollTop > 20) {
         $headerContainer.addClass('fixed');
       } else {
@@ -68,26 +105,45 @@ $(function(){
       }
       if (scrollTop < serviceTop) {
         curIndex = 0;
+        console.log("0");
       } else if (scrollTop < caseTop) {
         curIndex = 1;
+        console.log("1");
       } else if (scrollTop < teamTop) {
         curIndex = 2;
       } else {
         curIndex = 3;
       }
-      switchCur(curIndex);
-      $anchorParents.eq(curIndex).addClass('active').siblings('li').removeClass('active');
+      if ( preIndex != curIndex ) {
+        switchCur(curIndex);
+      }
+      if (!curFlag) {
+        $anchorParents.eq(curIndex).addClass('active').siblings('li').removeClass('active');
+      }
     }
 
     toggleScroll();
 
     $(window).scroll(toggleScroll);
 
-  })();
-});
-
-$(function () {
-  (function () {
+    // 滑动到锚链接
+    $anchors.click(function () {
+      var hr = $(this).attr("href");
+      var anh = $(hr).offset().top - 80;
+      var index = $anchors.index($(this));
+      var params = getParam(index);
+      curFlag = 1;
+      $curBar.animate({
+        left: params.curLeft,
+        width: params.curWidth
+      }, 400);
+      $("html,body").stop().animate({
+        scrollTop: anh
+      }, 1000, function(){
+        curFlag = 0;
+      });
+      return false;
+    });
 
   })();
 });
